c5072ebee39c052b5201e0efc139670a4316713f
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2024-09-15 14:53:42 -0300

    1.1

--- app.py
+++ app.py
@@ -8,10 +8,6 @@ app = Flask(__name__, template_folder="docs")
 def index():
     return render_template("index.html")
 
-@app.route("/stilo.css")
-def stilo():
-    return render_template("stilo.css")
-
 
 @app.route("/run", methods=["POST"])
 def run_code():
